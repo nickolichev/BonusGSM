7466ca433d2faefe1d9ad569f4331f5d653a7a23
nickolichev@gmail.com <nickolichev@gmail.com>
2022-04-30 00:19:49 +0300

    BonusGSM-4

--- src/Main.java
+++ src/Main.java
@@ -3,29 +3,26 @@ public class Main {
 
         int amountStart = 100; // начальная сумма на счете
         int amountRefillFirst = 100; // первое пополнение счета
-        int amountRefillSecond = 1_100; // второе пополнение счета
+        int amountRefillSecond = 1_000; // второе пополнение счета
+        int bonus1 = 0; // расчет бонусов
+        int totalFirst = (amountStart + amountRefillFirst + bonus1); // итог первого пополнения  счета
+        int bonus = (amountRefillSecond / 100); // расчет бонусов
+        int totalSecond = (amountStart + amountRefillSecond + bonus); // итог второго пополнения счета
 
+        if (amountRefillFirst < 1_000) {
 
-        if (amountRefillFirst < 1000) {
-
-            int bonus = 0; // расчет бонусов
-            int totalFirst = (amountStart + amountRefillFirst + bonus); // итог первого пополнения  счета
-
-            System.out.println("Сумма пополнения " + amountRefillFirst + " рублей");
-            System.out.println("Вам начислено " + bonus + " бонусных рублей");
-            System.out.println("Сумма на балансе " + totalFirst + " руб");
         }
 
-        if (amountRefillSecond > 1000) {
+        System.out.println("Сумма пополнения " + amountRefillFirst + " рублей");
+        System.out.println("Вам начислено " + bonus + " бонусных рублей");
+        System.out.println("Сумма на балансе " + totalFirst + " руб");
 
-            int bonus = (amountRefillSecond / 100); // расчет бонусов
-            int totalSecond = (amountStart + amountRefillSecond + bonus); // итог второго пополнения счета
+        if (amountRefillSecond >= 1_000) {
 
-            System.out.println("Сумма пополнения " + amountRefillSecond + " рублей");
-            System.out.println("Вам начислено " + bonus + " бонусных рублей");
-            System.out.println("Сумма на балансе " + totalSecond + " руб");
         }
-
+        System.out.println("Сумма пополнения " + amountRefillSecond + " рублей");
+        System.out.println("Вам начислено " + bonus + " бонусных рублей");
+        System.out.println("Сумма на балансе " + totalSecond + " руб");
 
     }
 }
